75fe51dee3d8669a06b88c3b4c6b5d0c4cb926b0
hamzashaukat <hamzashaukat2001@gmail.com>
2023-01-19 08:16:31 +0500

    DONE

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var sqlite3 = require("sqlite3").verbose();
 var express = require("express");
 var http = require("http");
 var path = require("path");
@@ -15,7 +14,7 @@ async function getConnection() {
   if (!con) {
     con = await oracledb.getConnection({
       user: "system",
-      password: "Oracle_5",
+      password: "Hamza123",
       connectString: "localhost/orcl",
     });
   }
@@ -26,50 +25,15 @@ async function getConnection() {
 const oracledb = require("oracledb");
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
-// async function fun() {
-/// i also removed this code bcz it was inserting name
-//and id khudi default wala everytime the node was running
-//   let con;
-
-// try {
-//   con = await oracledb.getConnection({
-//     user: "system",
-//     password: "Oracle_5",
-//     connectString: "localhost/orcl",
-//   });
-//     const data = await con.execute("SELECT * FROM USER_TT");
-
-//     console.log(data.rows);
-
-//     //insert data
-//     const sql = `insert into USER_TT (naam, numberr) values('hiii','hhh')`;
-//     // const sql = `insert into USER_T (naam, numberr) values(${nam},${okkk}')`;
-
-//     let result = await con.execute(sql);
-//     console.log(result.rowsAffected, "Rows Inserted");
-//     con.commit();
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-// fun();
-
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 150, // limit each IP to 100 requests per windowMs
 });
 
-var db = new sqlite3.Database("./database/employees.db");
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "./public")));
 app.use(helmet());
 app.use(limiter);
-
-db.run("CREATE TABLE IF NOT EXISTS emp(id TEXT, name TEXT)");
-
-// here gpt was saying do this
-//app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.static(path.join(__dirname, "/")));
 //this above code specifies where to load static/ css type files from.
@@ -97,7 +61,7 @@ app.post("/add", async function (req, res) {
         Emp_no: req.body.Emp_no,
         Emp_email: req.body.Emp_email,
         Manager_id: req.body.Manager_id,
-        Branch_id: req.body.Branch_id
+        Branch_id: req.body.Branch_id,
       }
     );
 
@@ -111,7 +75,7 @@ app.post("/add", async function (req, res) {
         " " +
         req.body.Emp_no +
         " " +
-        req.body.Emp_email+
+        req.body.Emp_email +
         " " +
         req.body.Manager_id +
         " " +
@@ -121,43 +85,6 @@ app.post("/add", async function (req, res) {
     console.error(err);
   }
 });
-
-// app.post("/add", async function (req, res) {
-//   const connection = await getConnection();
-
-//   try {
-//     const data = await connection.execute(
-//       "INSERT INTO USER_TTT(naam,numberr,Manager_id,Branch_id) VALUES(:naam,:numberr,:Manager_id,:Branch_id)",
-//       [
-//         req.body.naam,
-//         req.body.numberr,
-//         req.body.Manager_id,
-//         req.body.Branch_id,
-//       ],
-//       function (err) {
-//         if (err) {
-//           return console.log(err.message);
-//         }
-//         console.log("New employee has been added");
-//         res.send(
-//           "New employee has been added into the database with ID = " +
-//             req.body.naam +
-//             " and Name = " +
-//             req.body.numberr +
-//             " " +
-//             req.body.Manager_id +
-//             " " +
-//             req.body.Branch_id
-//         );
-//       }
-//     );
-
-//     connection.commit();
-//   } catch (err) {
-//     console.error(err);
-//   }
-// });
-///////////////////////////////////////////////////////////////////
 
 //after declaring con uper
 app.post("/view", async function (req, res) {
@@ -320,9 +247,21 @@ async function addToCart(
   } else {
     // Insert a new row into the cart table
     try {
+      // const data = await connection.execute(
+      //   "BEGIN add_product(:product_idd,:product_name,:price,:quantity,:itemID,:sizee,:customer_id); END;",
+      //   {
+      //     product_idd: product_idd,
+      //     product_name: product_name,
+      //     price: price,
+      //     quantity: quantity,
+      //     itemID: itemID,
+      //     sizee: sizee,
+      //     customer_id: customerId,
+      //   }
+      // );
       const data = await connection.execute(
-        "INSERT INTO Product(product_idd,product_name,price,quantity,itemID,sizee,customer_id) VALUES(:product_idd,:product_name,:price,:quantity,:itemID,:sizee,:customer_id)",
-        [product_idd,product_name, price, quantity, itemID, sizee, customerId],
+        "INSERT INTO Product(product_idd,price,quantity,itemID,sizee,customer_id) VALUES(:product_idd,:price,:quantity,:itemID,:sizee,:customer_id)",
+        [product_idd, price, quantity, itemID, sizee, customerId],
 
         function (err) {
           if (err) {
@@ -339,48 +278,6 @@ async function addToCart(
     }
   }
 }
-
-// async function addToCart(product_idd, price, quantity, itemID,sizee) {
-
-//   const connection = await getConnection();
-//   console.log("addToCart function called");
-//   console.log("productId:", product_idd);
-//   console.log("price:", price);
-//   console.log("quantity:", quantity);
-//   console.log("itemId:", itemID);
-
-//   console.log("size", sizee);
-
-//   // Check if the product already exists in the cart
-//   const exists = await checkIfProductExists(product_idd);
-//   if (exists) {
-//     // Update the quantity of the existing product
-//     await updateQuantity(product_idd, quantity);
-//   } else {
-//     // Insert a new row into the cart table
-//     try {
-//       const data = await connection.execute(
-
-//         "INSERT INTO Product(product_idd,price,quantity,itemID,sizee) VALUES(:product_idd,:price,:quantity,:itemID,:sizee)",
-//         [product_idd, price, quantity, itemID,sizee],
-
-//         function (err) {
-//           if (err) {
-//             return console.log(err.message);
-//           }
-//           console.log("New product has been added");
-
-//           console.log(`addCart function from customer id ${customerId}`);
-
-//         }
-//       );
-
-//       connection.commit();
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   }
-// }
 
 app.post("/add-to-cart", async function (req, res) {
   console.log("/add-to-cart route called");
@@ -400,31 +297,18 @@ app.post("/add-to-cart", async function (req, res) {
     await updateQuantity(product_idd, quantity);
   } else {
     // add new product to cart
-    await addToCart(product_idd,product_name, price, quantity, itemID, sizee, customerId);
+    await addToCart(
+      product_idd,
+      product_name,
+      price,
+      quantity,
+      itemID,
+      sizee,
+      customerId
+    );
   }
   res.send({ message: "Product added to cart successfully" });
 });
-// //inertion :
-// app.post("/add-to-cart", async function (req, res) {
-//   console.log("/add-to-cart route called");
-
-//   const product_idd = req.body.product_idd;
-//   const price = req.body.price;
-//   const quantity = req.body.quantity;
-//   const itemID = req.body.itemID;
-//   const sizee = req.body.sizee;
-
-//   // check if product already exists in cart
-//   const exists = await checkIfProductExists(product_idd);
-//   if (exists) {
-//     // update quantity if product already exists
-//     await updateQuantity(product_idd, quantity);
-//   } else {
-//     // add new product to cart
-//     await addToCart(product_idd, price, quantity, itemID,sizee);
-//   }
-//   res.send({ message: "Product added to cart successfully" });
-// });
 
 async function checkIfProductExists(product_idd) {
   const connection = await getConnection();
@@ -563,40 +447,6 @@ app.post("/cancel", async function (req, res) {
   }
 });
 
-//////entering customer details
-// app.post("/loginDetails", async function (req, res) {
-//   const connection = await getConnection();
-
-//   try {
-//     const data = await connection.execute(
-//       "INSERT INTO CUSTOME(Customer_id, Customer_pw, Customer_name, Customer_no, Customer_address) VALUES(:customerId, :customerPassword, :customerName, :customerNumber, :customerAddress)",
-//       [
-//         req.body.customerId,
-//         req.body.customerPassword,
-//         req.body.customerName,
-//         req.body.customerNumber,
-//         req.body.customerAddress,
-//       ],
-//       function (err) {
-//         if (err) {
-//           return console.log(err.message);
-//         }
-//         console.log("New customer has been added");
-//         res.send(
-//           "New customer has been added into the database with ID = " +
-//             req.body.customerId +
-//             " and Name = " +
-//             req.body.customerName
-//         );
-//       }
-//     );
-
-//     connection.commit();
-//   } catch (err) {
-//     console.error(err);
-//   }
-// });
-
 app.post("/loginDetails", async function (req, res) {
   const connection = await getConnection();
 
@@ -657,27 +507,31 @@ app.post("/login", async function (req, res) {
     console.error(err);
   }
 });
-// ////login
-// app.post("/login", async function (req, res) {
-//   const connection = await getConnection();
 
-//   try {
-//     const data = await connection.execute(
-//       "SELECT * FROM CUSTOME WHERE Customer_id = :customerId AND Customer_pw = :customerPassword",
-//       [req.body.customerId, req.body.customerPassword]
-//     );
-//     // check if data array is not empty, then redirect
-//     if (data.rows.length > 0) {
-//       console.log(`user with id just logged in`);
-//       res.redirect("/itemcatalog.html");
-//     } else {
-//       console.log("Invalidd credentials");
-//       res.send("Invalid credentials");
-//     }
-//   } catch (err) {
-//     console.error(err);
-//   }
-// });
+app.post("/login2", async function (req, res) {
+  const connection = await getConnection();
+  // let customerId;
+
+  try {
+    const data = await connection.execute(
+      "SELECT * FROM CUSTOME WHERE Customer_id = :customerId AND Customer_pw = :customerPassword",
+      [req.body.customerId, req.body.customerPassword]
+    );
+    // check if data array is not empty, then redirect
+    if (data.rows.length > 0) {
+      console.log(`user with id just logged in`);
+      customerId = req.body.customerId;
+      console.log(customerId);
+      //res.redirect("/itemcatalog.html");
+      res.redirect(`public/form.html#${req.body.customerId}`);
+    } else {
+      console.log("Invalidd credentials");
+      res.send("Invalid credentials");
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 ///RECEIPT VIEW:
 
